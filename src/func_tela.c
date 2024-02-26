9cf312f2f9497a0f4aa6da412f463bc72d40c90a
Osman Pereira de Oliveira <128541622+Osmanpdo@users.noreply.github.com>
2024-02-26 22:46:30 +0000

    deixando as tela mais faceis "amigaveis"

--- src/func_tela.c
+++ src/func_tela.c
@@ -48,42 +48,68 @@ float entradaDadosValidosReal(void){
 //Telas do software
 
 void telaMenuPrincipal(void){
-	int i;
-	char menu[149] = "=-=-=-=-=-=-=-=-=-=-=--=-=-=-=\nSeja bem-vindo(a) ao menu!\n[1]Jogos\n[2]Cálculos\n[3]Sair do programa\n=-=-=-=-=-=-=-=-=-=-=--=-=-=-=\nDigite aqui: ";
-	for (i = 0; i < 149; i++) printf("%c", menu[i]);	
+	printf("=-=-=-=-=-=-=-=-=-=-=--=-=-=-=\n");
+    printf("bem-vindo(a) ao menu!\n");
+    printf("[1]Jogos\n");
+    printf("[2]Cálculos\n");
+    printf("[3]Sair do programa\n");
+    printf("=-=-=-=-=-=-=-=-=-=-=--=-=-=-=\n");
+    printf("Digite aqui: ");
 }
 
 void menuCalculos(void){
-    int i;
-    char menu[261] = "=-=-=-=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=\nSeja bem-vindo(a) ao menu de cálculos\nEscolha uma das opções abaixo:\n\n[1]Calculadora simples\n[2]Conversor de medida simples\n[3]Calcular a área\n[4]Voltar ao menu\n=-=-=-=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=\nDigite aqui: ";
-    
-    for (i = 0; i < 261; i ++) printf("%c", menu[i]);    
+    printf("=-=-=-=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=\n");
+    printf("Seja bem-vindo(a) ao menu de cálculos\n");
+    printf("Escolha uma das opções abaixo:\n");
+    printf("[1]Calculadora simples\n");
+    printf("[2]Conversor de medida simples\n");
+    printf("[3]Calcular a área\n");
+    printf("[4]Voltar ao menu\n");
+    printf("=-=-=-=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=\n");
+    printf("Digite aqui: ");
 }
 
 void menuJogos(void){
-    int i;
-    char menu[193] = "=-=-=-=-=-=-=-=-=-=-=--=-=-=-=-=-=-=\nSeja bem-vindo(a) ao menu de jogos\n[1]Pedra, papel ou tesoura\n[2]Jogo de adivinha\n[3]Voltar ao menu\n=-=-=-=-=-=-=-=-=-=-=--=-=-=-=-=-=-=\nDigite aqui: ";
-    
-    for (i = 0; i < 193; i++) printf("%c", menu[i]);    
+    printf("=-=-=-=-=-=-=-=-=-=-=--=-=-=-=-=-=-=\n");
+    printf("Seja bem-vindo(a) ao menu de jogos\n");
+    printf("[1]Pedra, papel ou tesoura\n");
+    printf("[2]Jogo de adivinha\n");
+    printf("[3]Voltar ao menu\n");
+    printf("=-=-=-=-=-=-=-=-=-=-=--=-=-=-=-=-=-=\n");
+    printf("Digite aqui: "); 
 }
 
 void menuCaculadora(void){
-    int i;
-    char menu[256] = "=-=-=-=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=-=\nSeja bem-vindo(a) a calculadora simples!\nEscolha uma das operações abaixo:\n[1]Somar\n[2]Subtrair\n[3]Multiplicar\n[4]Dividir\n[5]Voltar ao menu de cálculos\n=-=-=-=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=-=\nDigite aqui: ";
-    
-    for (i = 0; i < 256; i ++) printf("%c", menu[i]);    
+    printf("=-=-=-=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=-=\n");
+    printf("Seja bem-vindo(a) a calculadora simples!\n");
+    printf("Escolha uma das operações abaixo:\n");
+    printf("[1]Somar\n");
+    printf("[2]Subtrair\n");
+    printf("[3]Multiplicar\n");
+    printf("[4]Dividir\n");
+    printf("[5]Voltar ao menu de cálculos\n");
+    printf("=-=-=-=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=-=\n");
+    printf("Digite aqui: ");
+  
 }
 
 void menuConversorMedida1(void){
-    int i;
-    char menu[222] = "=-=-=-=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=-=\nSeja bem-vindo(a) a conversor de medida\nsimples!\nEscolha uma das opções abaixo:\n[1]Metros\n[2]Decímetros\n[3]Centímetros\n=-=-=-=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=-=\nDigite aqui: ";
-    
-    for (i = 0; i < 222; i ++) printf("%c", menu[i]);    
+    printf("=-=-=-=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=-=\n");
+    printf("Seja bem-vindo(a) a conversor de medida simples!\n");
+    printf("Escolha uma das opções abaixo:\n");
+    printf("[1]Metros\n");
+    printf("[2]Decímetros\n");
+    printf("[3]Centímetros\n");
+    printf("=-=-=-=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=-=\n");
+    printf("Digite aqui: ");
 }
 
 void menuConversorMedida2(void){
-    int i;
-    char menu[66] = "Para...\n[1]metros\n[2]decímetros\n[3]centímetros\ndigite aqui: ";
-    
-    for (i = 0; i < 66; i ++) printf("%c", menu[i]);    
+    printf("=-=-=-=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=-=\n");
+    printf("Para...\n");
+    printf("[1]metros\n");
+    printf("[2]decímetros\n");
+    printf("[3]centímetros\n");
+    printf("=-=-=-=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=-=\n");
+    printf("digite aqui: "); 
 }
